9d8c99c2c58841b2cc1cbf033b6dcc1d5adbb088
mc <dandford@gmail.com>
2020-03-01 17:44:09 +0800

    Modify Challenge Statement

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,15 +9,12 @@ class StaticPagesController < ApplicationController
   end
 
   def challenge_statement_1
-    @challenge_statement = ChallengeStatement.find(1)
   end
 
   def challenge_statement_2
-    @challenge_statement = ChallengeStatement.find(2)
   end
 
   def challenge_statement_3
-    @challenge_statement = ChallengeStatement.find(3)
   end
 
 end
